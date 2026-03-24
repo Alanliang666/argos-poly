02e69b9e9e22b14dc92dee64c11e8d397d0f285a
Alanliang666 <95164876+Alanliang666@users.noreply.github.com>
2026-03-24 22:13:50 +0800

    feat(ws_client): refactor connection loop and add async queue - Implement while loop for connection and auto-reconnect mechanism - Add SSL context with certifi to resolve macOS certificate verification issues - Decouple data reception and processing using asyncio.Queue

--- src/ingestion/ws_client.py
+++ src/ingestion/ws_client.py
@@ -1,18 +1,38 @@
+"""
+TO DO: 
+- [Data Layer] Update `self.book_order` to maintain the latest Best Bid/Offer (BBO) as a real-time state shadow.
+- [Bridge] Use `self.queue.put_nowait(market_id)` to dispatch non-blocking update signals.
+- [Execute Layer] Create an independent async consumer loop (`await queue.get()`) to evaluate strategy rules purely against the freshest `self.book_order` state, bypassing stale ticks, and execute trades instantly.
+"""
+
 import asyncio
 import websockets
 import json
+import certifi
+import ssl
 
 class WsClient:
     def __init__(self, market_info, url):
         # get the market info into this function
         self.market_info = market_info
         self.url = url
+        self.book_order = {}
         self.book_order["market_id"] = {}
         self.queue = asyncio.Queue()
 
     async def ws_connect(self):
-        async with websockets.connect(self.url) as ws:
-            await ws.send(self._build_subscription_payload())
+        ssl_context = ssl.create_default_context(cafile=certifi.where())
+        while True:
+            try:
+                async with websockets.connect(self.url, ping_interval=10, ping_timeout=10, ssl=ssl_context) as ws:
+                    await ws.send(self._build_subscription_payload())
+                    while True:
+                        message = await ws.recv()
+                        await self.queue.put(message)
+
+            except Exception as e:
+                # if return error then we stop 5 sencond for next round
+                await asyncio.sleep(5)
 
     def _build_subscription_payload(self):
         send_json = {"assets_ids":list(self.market_info),
@@ -30,3 +50,18 @@ class WsClient:
                 if asset_id in self.market_info:
                     market_id, question, outcome = self.market_info[asset_id]
                     print(f"Event {question} -> {outcome} ask update -> {change.get('best_bid')}") 
+
+
+# test case for develope
+# async def main():
+#     dummy_market_info = {
+#         "1234567890": ("MockMarket", "Will BTC reach 100k?", "Yes"),
+#         "0987654321": ("MockMarket", "Will BTC reach 100k?", "No")
+#     }
+#     client = WsClient(dummy_market_info,'wss://ws-subscriptions-clob.polymarket.com/ws/market')
+#     asyncio.create_task(client.ws_connect())
+#     while True: 
+#         print(await client.queue.get())
+
+# if __name__ == "__main__":
+#     asyncio.run(main())
